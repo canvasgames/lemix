74a121fb22d6b9c839d66253a492384a105b348d
canvasgames <eduardo@canvasmobilegames.com>
2015-12-05 00:18:06 -0200

    - Tutorial, the beggining

---
+++ Assets/Full_Screen_Dialog.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Full_Screen_Dialog : MonoBehaviour {
+
+    float xPos;
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void act()
+    {
+        xPos = transform.position.x;
+        transform.position = new Vector3((xPos + 500f), transform.position.y, 0f);
+        gogogogogo();
+    }
+
+    void gogogogogo()
+    {
+        // 
+        transform.DOMoveX(xPos, 1f).OnComplete(punch);
+    }
+
+    void punch()
+    {
+        transform.DOPunchPosition(new Vector3(-2f, 0f, 0f), 0.5f, 7, 0.9f);
+    }
+}
--- Assets/Inferno Manager/Scripts/Controllers/TutorialController.cs
+++ Assets/Inferno Manager/Scripts/Controllers/TutorialController.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour {
     public static TutorialController s;
+    GameObject tempObject;
+    public Transform bigDaddy;
+
+    float tutorial1Timer;
 
     void Awake()
     {
@@ -11,11 +17,83 @@ public class TutorialController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-	
-	}
+        int firstGame = PlayerPrefs.GetInt("firstGame");
+        Debug.Log(firstGame);
+
+        if(firstGame == 0)
+        {
+           // PlayerPrefs.SetInt("firstGame", 1);
+            tutorial1Timer = 2f;
+        }
+            
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (tutorial1Timer > 0)
+        {
+            tutorial1Timer -= Time.deltaTime;
+            if(tutorial1Timer <= 0 )
+            {
+                tutorial1();
+                tutorial1Timer = 0;
+            }
+
+        }
 	}
+
+    void tutorial1()
+    {
+
+        tempObject = (GameObject)Instantiate(Resources.Load("Prefabs/FullScreenDialog"));
+        addToGUIAndRepositeObject(tempObject);
+        tempObject.GetComponent<Full_Screen_Dialog>().act();
+
+        tempObject = (GameObject)Instantiate(Resources.Load("Prefabs/Satan"));
+        addToGUIAndRepositeObject(tempObject);
+        tempObject.GetComponent<Satan_HUD>().act();
+
+    }
+
+    void desappear(GameObject desappear)
+    {
+
+        Image[] images = desappear.GetComponentsInChildren<Image>();
+
+        foreach (Image myImg in images)
+        {
+            myImg.DOFade(0f, 0f);
+
+        }
+        appear(desappear, 1f);
+
+    }
+
+    void appear(GameObject appear, float time)
+    {
+
+        Image[] images = appear. GetComponentsInChildren<Image>();
+        
+        foreach (Image myImg in images)
+        {
+            myImg.DOFade(1f, 2f);
+
+        }
+        
+    }
+
+    void addToGUIAndRepositeObject(GameObject menu)
+    {
+        //Copy thereated local position
+        float xPos, yPos;
+        xPos = menu.transform.localPosition.x;
+        yPos = menu.transform.localPosition.y;
+
+        //Set the Canvas of GUI was parent
+        menu.transform.SetParent(bigDaddy);
+
+        //Set again the local position, now in the GUI
+        menu.transform.localPosition = new Vector3(xPos, yPos, 0f);
+    }
 }
