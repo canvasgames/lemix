8d3ee1e79167ff5bf5f7b01f2cb581ff5cd061b8
canvasgames <eduardo@canvasmobilegames.com>
2016-11-12 04:41:05 -0200

    - pitiça

---
+++ Super Casual/Assets/Demigiant/DOTween/Editor/MenusEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Collections;
+
+public class MenusEditor
+{
+
+    [MenuItem("Tools/Clear PlayerPrefs")]
+    private static void NewMenuOption()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
